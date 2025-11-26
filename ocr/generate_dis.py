0a82324a0e75ec3d208a075489b2159ce0c8a3cb
tsctw <152745979+tsctw@users.noreply.github.com>
2025-11-26 20:43:35 +0000

    new model

--- ocr/generate_dis.py
+++ ocr/generate_dis.py
@@ -2,42 +2,17 @@ import os
 import random
 import string
 from PIL import Image, ImageDraw, ImageFont
-from png_to_base64 import make_distorted_png
+from png_to_base64 import make_captcha_media
 import base64
 from io import BytesIO
 
 FONT_PATH = "/Users/taosenchang/tcd_scalable_computing/project2/attackers_new/ocr/Kablammo.ttf"
 
 def generate_captcha(word, output_path):
-    width, height = 150, 60
-    img = Image.new("RGB", (width, height), (255, 255, 255))
-    d = ImageDraw.Draw(img)
+    
+    b64_str = make_captcha_media(word)
 
-    size = random.randint(28, 36)
-    font = ImageFont.truetype(FONT_PATH, size)
-
-    for _ in range(150):
-        d.point(
-            (random.randint(0, width-1), random.randint(0, height-1)),
-            fill=(
-                random.randint(100, 200),
-                random.randint(100, 200),
-                random.randint(100, 200)
-            )
-        )
-
-    text_x = 40
-    text_y = (height - size) // 2
-
-    d.text(
-        (text_x, text_y),
-        word,
-        font=font,
-        fill=(48, 115, 240) 
-    )
-    b64_str = make_distorted_png(word)
-
-     # Remove header like: "data:image/png;base64,"
+     # Remove header like: "data:image/webp;base64,"
     if "," in b64_str:
         b64_str = b64_str.split(",")[1]
 
@@ -46,20 +21,20 @@ def generate_captcha(word, output_path):
     img = Image.open(BytesIO(image_data))
 
     # Resize using Pillow new API
-    img = img.resize((width, height), Image.Resampling.LANCZOS)
+    img = img.resize((260, 80), Image.Resampling.LANCZOS)
 
     img.save(output_path)
 
 
 symbols_path = 'symbols.txt'
-DEST = 'test_data5'
+DEST = 'train_data'
 
 with open(symbols_path, 'r') as f:
     SYMBOLS = f.readline().strip()
 
 if __name__ == "__main__":
     os.makedirs(DEST, exist_ok=True)
-    for i in range(1000):
+    for i in range(50000):
         s = "".join(random.choice(SYMBOLS) for _ in range(4))
         generate_captcha(s, f"{DEST}/{s}.png")
     
--- ocr/png_to_base64.py
+++ ocr/png_to_base64.py
@@ -4,23 +4,74 @@ import io
 
 FONT_PATH = "/Users/taosenchang/tcd_scalable_computing/project2/attackers_new/ocr/Kablammo.ttf"
 
-def make_distorted_png(word):
-    img = Image.new("RGB", (150, 60), (255, 255, 255))
-    d = ImageDraw.Draw(img)
+CHALLENGES = {}
+MAX_WORD_COUNT = 4
+SIZE = 60
 
-    size = random.randint(28, 36)
-    font = ImageFont.truetype(FONT_PATH, size)
+def make_letter_tile(letter, color=(0,0,0)):
+    img = Image.new("RGBA",(SIZE, SIZE),(255,255,255,0))
 
-    # random noise
-    for _ in range(150):
-        d.point(
-            (random.randint(0,149), random.randint(0,59)),
-            fill=(random.randint(0,200),random.randint(0,200),random.randint(0,200))
-        )
+    # letter interior mask
+    mask = Image.new("L",(SIZE, SIZE),0)
+    dmask = ImageDraw.Draw(mask)
+    font = ImageFont.truetype(FONT_PATH, 42)
 
-    d.text((40,10), word, font=font, fill=(48, 115, 240))
+    dmask.text((20,0), letter, font=font, fill=255)
+
+    # fill interior with granular noise
+    for _ in range(4000):
+        x = random.randint(0, SIZE-1)
+        y = random.randint(0, SIZE-1)
+
+        if mask.getpixel((x,y)) > 180:
+            img.putpixel(
+                (x,y),
+                (
+                    min(color[0] + random.randint(-20,20),255),
+                    min(color[1] + random.randint(-20,20),255),
+                    min(color[2] + random.randint(-20,20),255),
+                    240
+                )
+            )
+
+    return img
+
+def make_captcha_media(word, animated=True):
+
+    FRAMES = 4
+    DURATION = 120
+
+    frames = []
+
+    for _ in range(FRAMES):
+
+        final = Image.new("RGB", (260, 80), (255,255,255))
+        d = ImageDraw.Draw(final)
+
+        # paste tiles
+        x = 10
+        for ch in word:
+            tile = make_letter_tile(ch)
+            final.paste(tile, (x, random.randint(0,25)), tile)
+            x += SIZE
+
+        frames.append(final)
 
     buf = io.BytesIO()
-    img.save(buf, format='webp')
-    b64 = base64.b64encode(buf.getvalue()).decode('utf8')
-    return f"data:image/png;base64,{b64}"
+    
+    # static WEBP
+    if not animated:
+        frames[0].save(buf, format="WEBP")
+    else:
+        # animated GIF
+        frames[0].save(
+            buf,
+            format='webp',
+            save_all=True,
+            append_images=frames[1:],
+            duration=DURATION,
+            optimize=False,
+            loop=0
+        )
+
+    return "data:image/webp;base64," + base64.b64encode(buf.getvalue()).decode()
