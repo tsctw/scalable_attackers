6218d47b8d5da1228faa2e0bd4dbfcb9a93269c4
tsctw <152745979+tsctw@users.noreply.github.com>
2025-11-26 16:14:42 +0000

    update

--- attacker1.py
+++ attacker1.py
@@ -1,24 +1,36 @@
 import requests
 
+from metrics import METRICS
 from ocr.base64_to_png import base64_to_png
 from ocr.classify import classify_image
 
 def solve_captcha():
     client = requests.Session()
     
-    session = client.post("http://127.0.0.1:5000/start_session").json()
+    session = client.post("http://127.0.0.1:5000/start_session", headers={"X-Forwarded-For": "8.8.8.8"}).json()
+    # print(session)
     session_id = session["session_id"]
-    print(session_id)
+    # print(session_id)
 
     captcha1 = client.get(f"http://localhost:5055/get_challenge?sessionId={session_id}").json()
-    print(captcha1)
+    # print(captcha1)
+
+    target_list = []
+
+    for elem in captcha1['randomLetters']:
+        target_list.append(elem['letter'])
+
+    target_list.sort()
+
+    target = ''.join(target_list)
 
     # generate png file
-    base64_to_png(captcha1["svgImg"], 'output.png')
+    # base64_to_png(captcha1["svgImg"], 'output.png')
 
-    target = classify_image("test", "symbols.txt", "output.png")
+    # target = classify_image("test", "symbols.txt", "output.png")
 
-    print(target)
+    # print(target)
+    
 
     # ---------------------------
     verify_payload = {
@@ -32,27 +44,8 @@ def solve_captcha():
     # ---------------------------
     main_verify_payload = {
         "session_id": session_id,
-        "user_answer": target,
-        "correct_word": captcha1["randomLetters"],
         "status": "passed",
-        "metrics": {
-            "reaction_time_mean_ms": 1922,
-            "solve_time_std_ms": 199776.04999995232,
-            "interkey_interval_std_ms": 1,
-            "path_entropy": 0.5751942410279781,
-            "velocity_std_px_per_s": 303.39539360134313,
-            "click_offset_avg_px": 161.23409208103513,
-            "hover_dwell_avg_ms": 319.68499999841055,
-            "backspace_count": 1,
-            "solve_entropy": 199776.04999995232,
-            "entry_points_unique": 66,
-            "focus_change_events": 2,
-            "swipe_accel_var": 1,
-            "pause_variance_ms": 1,
-            "pressure_std": 1,
-            "fingerprint_entropy": 1,
-            "overall_variance_score": 199776.04999995232
-        }
+        "metrics": METRICS
     }
 
     headers = {
